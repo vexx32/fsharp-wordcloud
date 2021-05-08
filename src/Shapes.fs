4cb9821239ba6b540f9d0a0c593b0a7268c55b20
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2021-05-08 00:30:32 -0400

    :recycle: Fix casing

--- src/Shapes.fs
+++ src/Shapes.fs
@@ -6,14 +6,15 @@ open Utils
 open Extensions
 
 module Shapes =
-    type Ellipse = { semiMinorAxis : float32; aspectRatio : float32; centre : SKPoint }
+    [<Struct>]
+    type Ellipse = { SemiMinorAxis : float32; AspectRatio : float32; Centre : SKPoint }
 
     let private random = Random()
 
     let private getPointOnEllipse ellipse (angle : int) =
-        let complexPoint = Complex.FromPolarCoordinates(float ellipse.semiMinorAxis, angle.ToRadians)
-        let x = ellipse.centre.X + float32 complexPoint.Real * ellipse.aspectRatio
-        let y = ellipse.centre.Y + float32 complexPoint.Imaginary
+        let complexPoint = Complex.FromPolarCoordinates(float ellipse.SemiMinorAxis, angle.ToRadians)
+        let x = ellipse.Centre.X + float32 complexPoint.Real * ellipse.AspectRatio
+        let y = ellipse.Centre.Y + float32 complexPoint.Imaginary
 
         new SKPoint(x, y)
 
@@ -24,17 +25,17 @@ module Shapes =
     /// <param name="radialStep">The step radial distance between each point.</param>
     /// <returns>A list of points that comprise the ellipse. If the primary axis of the ellipse is <c>0</c> then the only point in the list will be its centre.</returns>
     let getEllipsePoints ellipse radialStep =
-            if (ellipse.semiMinorAxis = 0f) then
-                [ ellipse.centre ]
+            if (ellipse.SemiMinorAxis = 0f) then
+                [ ellipse.Centre ]
             else 
                 let startingAngle = random.Next(0, 5) * 90
                 let clockwise = random.NextDouble() > 0.5
                 
                 let increment = 
                     if clockwise then
-                        getAngleIncrement ellipse.semiMinorAxis radialStep
+                        getAngleIncrement ellipse.SemiMinorAxis radialStep
                     else
-                        - (getAngleIncrement ellipse.semiMinorAxis radialStep)
+                        - (getAngleIncrement ellipse.SemiMinorAxis radialStep)
 
                 let maxAngle = 
                     if clockwise then
